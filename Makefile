0a741cc907c684e6a8af4bc34c1f36ecd0ae7352
wahyu agus arifin <itpohgero@gmail.com>
2025-02-01 18:33:50 +0700

    add make file

---
+++ Makefile
@@ -0,0 +1,6 @@
+# Makefile
+
+# Target untuk menjalankan skrip push.sh
+push:
+	@echo "🚀 Running push.sh..."
+	@./shell/push.sh
---
+++ shell/push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Function to run a command and handle errors
+run_command() {
+    echo "Running: $1"
+    if ! eval "$1"; then
+        echo "Error running command: $1"
+        exit 1
+    fi
+}
+
+# Run lint, check, and format
+# run_command "bun run biome"
+
+# Stage changes (excluding specific files, like `.env`, if needed)
+run_command "git add ."
+
+# Prompt for commit message
+read -r -p "Enter commit message: " msg
+
+# Commit changes
+run_command "git commit -m \"$msg\""
+
+# Push changes
+run_command "git push"

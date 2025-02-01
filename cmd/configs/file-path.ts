b636b2a00f5f4b2546f3a97e1c3aa4edb79f4e7e
wahyu agus arifin <itpohgero@gmail.com>
2025-02-01 23:23:46 +0700

    yey

--- cmd/configs/file-path.ts
+++ cmd/configs/file-path.ts
@@ -1,36 +1,19 @@
 import { join } from "node:path";
 import { file } from "bun";
 
+const getFilePath = (fileName: string) =>
+	join(process.env.HOME || "", ".local", "share", "lokio", fileName);
+
 export const filePaths = {
-	"info.md": join(
-		process.env.HOME || "",
-		".local",
-		"share",
-		"lokio",
-		"info.md",
-	),
-	"astro-frontend.yaml": join(
-		process.env.HOME || "",
-		".local",
-		"share",
-		"lokio",
-		"astro-frontend.yaml",
-	),
-	"go-backend.yaml": join(
-		process.env.HOME || "",
-		".local",
-		"share",
-		"lokio",
-		"go-backend.yaml",
-	),
-	"hono-backend.yaml": join(
-		process.env.HOME || "",
-		".local",
-		"share",
-		"lokio",
-		"hono-backend.yaml",
-	),
+	"info.md": getFilePath("info.md"),
+	"astro-frontend.yaml": getFilePath("astro-frontend.yaml"),
+	"go-backend.yaml": getFilePath("go-backend.yaml"),
+	"hono-backend.yaml": getFilePath("hono-backend.yaml"),
+	"kt-mobile-compose-mvvm.yaml": getFilePath("kt-mobile-compose-mvvm.yaml"),
+	"next-frontend.yaml": getFilePath("next-frontend.yaml"),
+	"next-monolith.yaml": getFilePath("next-monolith.yaml"),
 };
+
 const EMBEDDED_FILES: { [key: string]: string } = {};
 
 for (const [key, path] of Object.entries(filePaths)) {

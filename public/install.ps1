919fdfad03c3d4fa3b76e2fdbad0a6dce630d3c4
wahyu agus arifin <itpohgero@gmail.com>
2025-02-02 14:42:38 +0700

    up

--- public/install.ps1
+++ public/install.ps1
@@ -1,5 +1,5 @@
 # Configuration
-$binaryUrl = "http://103.127.96.116:9000/bin/windows.exe"
+$binaryUrl = "https://sh.lokio.dev/bin/windows.exe"
 $installDir = "$env:ProgramFiles\Lokio"
 $maxRetries = 3
 $retryCount = 0

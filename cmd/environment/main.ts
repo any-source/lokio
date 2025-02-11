184847ec5774656a4012a842de2074123281e971
wahyu agus arifin <itpohgero@gmail.com>
2025-02-11 15:21:02 +0700

    1.0.6

--- cmd/environment/main.ts
+++ cmd/environment/main.ts
@@ -3,6 +3,7 @@ import pkg from "../../package.json" assert { type: "json" };
 export const ENV = {
 	NAME: "lokio",
 	AUTHOR: pkg.author.name,
+	VERSION: pkg.version,
 	TAGLINE: "Structuring Code, One Command at a Time",
 	CONFIG_FILE_NAME: ".lokio.yaml",
 	LOKIO_REGISTRY: "https://registry.npmjs.org/lokio-assistant/latest",
